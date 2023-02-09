95b0c6d4d14b13754d6470190836dcb28ef40b29
puskkar more <puskkar.more@accenture.com>
2023-02-09 14:54:38 +0100

    changesets

--- lib/eks-cluster-stack.ts
+++ lib/eks-cluster-stack.ts
@@ -18,53 +18,10 @@ export class EksClusterStack extends cdk.Stack {
     const cluster = new eks.Cluster(this, `acme-${props.nameSuffix}`, {
       clusterName: `acme-${props.nameSuffix}`,
       version: props.clusterVersion,
-      defaultCapacity: 2,
+      defaultCapacity: 0,
       vpc,
       vpcSubnets: [{ subnetType: ec2.SubnetType.PRIVATE }],
     });
 
-    const aud = `${cluster.clusterOpenIdConnectIssuer}:aud`;
-    const sub = `${cluster.clusterOpenIdConnectIssuer}:sub`;
-
-    const conditions = new cdk.CfnJson(this, "awsNodeOIDCCondition", {
-      value: {
-        [aud]: "sts.amazonaws.com",
-        [sub]: "system:serviceaccount:kube-system:aws-node",
-      },
-    });
-
-    const awsNodeIamRole = new iam.Role(this, "awsNodeIamRole", {
-      assumedBy: new iam.WebIdentityPrincipal(
-        `arn:aws:iam::${cdk.Aws.ACCOUNT_ID}:oidc-provider/${cluster.clusterOpenIdConnectIssuer}`
-      ).withConditions({
-        StringEquals: conditions,
-      }),
-    });
-
-    awsNodeIamRole.addManagedPolicy(
-      iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonEKS_CNI_Policy")
-    );
-
-    const awsNodeCniPatch = new eks.KubernetesPatch(
-      this,
-      "serviceAccount/aws-node",
-      {
-        cluster,
-        resourceName: "serviceAccount/aws-node",
-        resourceNamespace: "kube-system",
-        applyPatch: {
-          metadata: {
-            annotations: {
-              "eks.amazonaws.com/role-arn": awsNodeIamRole.roleArn,
-            },
-          },
-        },
-        restorePatch: {
-          metadata: {
-            annotations: {},
-          },
-        },
-      }
-    );
   }
 }
