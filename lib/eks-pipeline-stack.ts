b7a9d0045ac6ebe2ddcb41a35139f0167a4d15b6
puskkar more <puskkar.more@accenture.com>
2023-02-10 16:48:30 +0100

    changes

--- lib/eks-pipeline-stack.ts
+++ lib/eks-pipeline-stack.ts
@@ -19,20 +19,23 @@ const app = new cdk.App();
 const stack = new cdk.Stack(app, 'MyStack');
 
 
-// Read the secret from Secrets Manager
-const pipeline = new codepipeline.Pipeline(this, 'MyPipeline');
-const sourceOutput = new codepipeline.Artifact();
-const sourceAction = new codepipelineActions.GitHubSourceAction({
-  actionName: 'GitHub_Source',
-  owner: 'awslabs',
-  repo: 'aws-cdk',
-  oauthToken: SecretValue.secretsManager('my-github-token'),
-  output: sourceOutput,
-  branch: 'develop', // default: 'master'
+
+const pipeline = new CodePipeline(this, "Pipeline", {
+  synth: new ShellStep("Synth", {
+    input: CodePipelineSource.gitHub(
+      "puskkarmore24/aws-cdk-pipelines-eks-cluster-main",
+      "main",
+      {
+        authentication:
+          cdk.SecretValue.secretsManager("github-oauth-token"),
+      }
+    ),
+    commands: ["npm ci", "npm run build", "npx cdk synth"],
+  }),
+  pipelineName: "EKSCluster",
 });
-pipeline.addStage({
-  stageName: 'Source',
-  actions: [sourceAction],
-});
+
+
+
   }
 }
