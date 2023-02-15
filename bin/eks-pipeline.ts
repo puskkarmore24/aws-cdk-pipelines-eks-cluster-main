13d0874056c06e489dd8cb2c82d3916a52542368
puskkar more <puskkar.more@accenture.com>
2023-02-15 23:30:55 +0100

    changes commit

--- bin/eks-pipeline.ts
+++ bin/eks-pipeline.ts
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import { EksPipelineStack } from '../lib/eks-pipeline-stack';
-import * as cdk from '@aws-cdk/core';
+import * as cdk from 'aws-cdk-lib/core';
+import { App } from 'aws-cdk-lib/core';
 
-const app = new cdk.App();
+const app = new App();
 new EksPipelineStack(app, 'EksPipelineStack', {
   env: { account: '861976376325', region: 'eu-north-1' },
 });
--- lib/eks-pipeline-stack.ts
+++ lib/eks-pipeline-stack.ts
@@ -1,36 +1,32 @@
-import * as cdk from "@aws-cdk/core";
-import eks = require("@aws-cdk/aws-eks");
-import {
-  CodePipeline,
-  CodePipelineSource,
-  ShellStep,
-} from "@aws-cdk/pipelines";
-import { EksClusterStage } from "./eks-cluster-stage";
-import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
-import * as codepipelineActions from 'aws-cdk-lib/aws-codepipeline-actions';
-import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
-import { SecretValue } from "@aws-cdk/core";
+import { SecretValue, Stack, StackProps } from "aws-cdk-lib";
+import { CodePipeline, CodePipelineSource, ShellStep } from "aws-cdk-lib/pipelines";
+import { Construct } from "constructs";
+import {EksClusterStage} from "../lib/eks-cluster-stage";
 
-export class EksPipelineStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+export class EksPipelineStack extends Stack {
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
 
-const pipeline = new CodePipeline(this, "Pipeline", {
-  synth: new ShellStep("Synth", {
-    input: CodePipelineSource.gitHub(
-      "puskkarmore24/aws-cdk-pipelines-eks-cluster-main",
-      "main",
-      {
-        authentication:
-          cdk.SecretValue.secretsManager("github-oauth-token"),
-      }
-    ),
-    commands: ["npm ci", "npm run build", "npx cdk synth"],
-  }),
-  pipelineName: "EKSCluster",
-});
+        const pipeline = new CodePipeline(this, "EksDeploymentPipeline", {
+            pipelineName: "Eks-Pipeline",
+            synth: new ShellStep("Synth", {
+                input: CodePipelineSource.gitHub(
+                    "puskkarmore24/aws-cdk",
+                    "main",
+                    {
+                      authentication:
+                        SecretValue.secretsManager("github-token"),
+                    }
+                  ),
+                commands: [
+                    "cd cdk",
+                    "npm ci",
+                    "npm run build",
+                    "npx cdk synth"
+                ],
+                primaryOutputDirectory: "cdk/cdk.out"
+            })
 
-
-
-  }
+        });
+    }
 }
